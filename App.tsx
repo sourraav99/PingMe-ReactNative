86089fe8d16ed43c28123bc81631739b4f012b2b
sourabh singh <sourabhsingh@sourabhs-Mac-mini.local>
2025-07-09 17:07:40 +0530

    push notification done

--- App.tsx
+++ App.tsx
@@ -1,28 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { View, Text, PermissionsAndroid, Alert } from 'react-native'
+import React, { useEffect } from 'react'
+import messaging from '@react-native-firebase/messaging';
+import notifee from '@notifee/react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+const App = () => {
+  useEffect(() => {
+    requestAndroidPermissions();
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      // Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      onDisplayNotification(remoteMessage);
+    });
+    return unsubscribe;
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  }, [])
+
+  const getToken = async () => {
+    const token = await messaging().getToken();
+    console.log('TOKEN:', token);
+
+  }
+
+  const requestAndroidPermissions = async () => {
+    const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      // Alert.alert('Notification Permission Granted');
+      getToken();
+    } else {
+      // Alert.alert('Notification Permission Denied');
+    }
+
+  }
+
+  const onDisplayNotification = async (remoteMessage: any) => {
+
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: remoteMessage.notification.title,
+      body: remoteMessage.notification.body,
+      android: {
+        channelId,
+        sound: 'snapnotifi',
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
+
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View>
+      <Text>App</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+export default App
